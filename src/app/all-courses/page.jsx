29e506c19f4b75d7e68bf42927fc417b7dd11490
Mihir Suman <mihirsuman0131@gmail.com>
2023-08-07 16:47:45 +0530

    recent-courses-added

--- src/app/all-courses/page.jsx
+++ src/app/all-courses/page.jsx
@@ -1,12 +1,24 @@
+import React from "react";
 import Cards from "../../components/Cards";
 
 export default function AllCoursesPage() {
 	return (
-		<main className="">
-			<h1 className="font-mono font-bold text-3xl pl-20 p-4 m-4">
-				All Courses
-			</h1>
-			<Cards />
-		</main>
+		<div className="h-1/2 flex flex-col">
+			{/* All Courses Section */}
+			<div className="flex-1 flex flex-col">
+				<h1 className="font-mono font-bold text-3xl pl-20 p-3.5 m-3.5">
+					All Courses
+				</h1>
+				<Cards />
+			</div>
+
+			{/* Recent Courses Section */}
+			<div className="flex-1 flex flex-col">
+				<h1 className="font-mono font-bold text-3xl pl-20 p-4 m-4">
+					Recent Courses
+				</h1>
+				<Cards />
+			</div>
+		</div>
 	);
 }
